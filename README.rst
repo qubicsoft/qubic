a758a88068cb4e1328363f4701091f9cf249256d
chanial <chanial@a6c58dc5-0cb5-4991-9159-8e83d7052c70>
2013-05-21 14:02:31 +0000

    Add files to make qubic a distribution.
    git-svn-id: svn+ssh://svn.in2p3.fr/qubic@10 a6c58dc5-0cb5-4991-9159-8e83d7052c70

---
+++ README.rst
@@ -0,0 +1,1 @@
+Simulation and map-making tools for the QUBIC experiment.
---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import os
+import re
+from distutils.core import setup
+
+def version():
+    f = open(os.path.join('qubic', 'version.py')).read()
+    m = re.search(r"VERSION = '(.*)'", f)
+    return m.groups()[0]
+
+version = version()
+long_description = open('README.rst').read()
+keywords = 'scientific computing'
+platforms = 'MacOS X,Linux,Solaris,Unix,Windows'
+
+setup(name='qubic',
+      version=version,
+      description='Simulation and map-making tools for the QUBIC experiment.',
+      long_description=long_description,
+      url='',
+      author='Pierre Chanial',
+      author_email='pierre.chanial@apc.univ-paris7.fr',
+      install_requires=['pysimulators>=0.3', 'healpy>=0.6.1'],
+      packages=['qubic'],
+      platforms=platforms.split(','),
+      keywords=keywords.split(','),
+      license='CeCILL-B',
+      classifiers = [
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2 :: Only',
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Science/Research',
+          'Operating System :: OS Independent',
+          'Topic :: Scientific/Engineering',
+          'Topic :: Scientific/Engineering :: Astronomy',
+          ])

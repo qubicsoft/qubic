993dc7d7d713929872b635c8fea3c6342413a5bf
Pierre Chanial <pierre.chanial@gmail.com>
2014-09-24 21:57:55 +0200

    Update hooks.py.

--- setup.py
+++ setup.py
@@ -2,12 +2,9 @@
 import numpy as np
 import os
 import sys
+import hooks
 from numpy.distutils.core import setup
 from numpy.distutils.extension import Extension
-from hooks import get_cmdclass, get_version
-
-# force sdist to copy files
-delattr(os, 'link')
 
 VERSION = '4.3'
 
@@ -18,26 +15,20 @@ platforms = 'MacOS X,Linux,Solaris,Unix,Windows'
 extra_f90_compile_args = ['-g -cpp -fopenmp -fpack-derived']
 if '--debug' in sys.argv:
     extra_f90_compile_args += ['-fbounds-check']
+delattr(os, 'link')  # force sdist to copy files
+hooks.REQUIRES_FORTRAN = True
 
-if any(c in sys.argv for c in ('build', 'build_ext', 'install')):
-    # write f2py's type mapping file
-    root = os.path.dirname(__file__)
-    with open(os.path.join(root, '.f2py_f2cmap'), 'w') as f:
-        f.write("{'integer': {'int8': 'char', 'int16': 'short', 'int32': 'int', 'int64': 'long_long'},"
-                " 'real': {'sp': 'float', 'dp': 'double', 'p': 'double', 'real32': 'float', 'real64': 'double'},"
-                " 'complex': {'sp': 'complex', 'dp': 'complex_double', 'p': 'complex_double'}}\n")
 
 ext_modules = [Extension('qubic._flib',
                          sources=['src/polarization.f90.src',
                                   'src/xpol.f90'],
                          extra_f90_compile_args=extra_f90_compile_args,
-                         f2py_options=['--quiet'],
                          include_dirs=['.', np.get_include()],
                          libraries=['gomp',
                                     ('fmod', {'sources': ['src/wig3j.f']})])]
 
 setup(name=name,
-      version=get_version(name, VERSION),
+      version=hooks.get_version(name, VERSION),
       description='Simulation and map-making tools for the QUBIC experiment.',
       long_description=long_description,
       url='',
@@ -52,7 +43,7 @@ setup(name=name,
       package_data={'qubic': ['calfiles/*fits', 'data/*', 'scripts/*py']},
       platforms=platforms.split(','),
       keywords=keywords.split(','),
-      cmdclass=get_cmdclass(),
+      cmdclass=hooks.get_cmdclass(),
       ext_modules=ext_modules,
       license='CeCILL-B',
       classifiers=[

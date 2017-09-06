Correct bug for polyacquisition No weight when averaging the monochromatic acquisitions

qubic/polyacquisition.py
@@ -89,7 +89,7 @@ class QubicPolyAcquisition(object):
             a.comm = self[0].comm
         self.scene = scene
         if weights == None:
-            self.weights = np.ones(len(self)) / len(self)
+            self.weights = np.ones(len(self)) #/ len(self)
         else:
             self.weights = weights
 
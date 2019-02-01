14136d33e8e0fbd8b39fd014ca735538ad64440a
Jean-Christophe Hamilton <hamilton@eduroam-prg-cd-1-9-58.net.univ-paris-diderot.fr>
2019-02-01 15:37:19 +0100

    some modif

--- qubic/scripts/Calibration/scan_az.py
+++ qubic/scripts/Calibration/scan_az.py
@@ -8,7 +8,9 @@ import glob
 from qubicpack import qubicpack as qp
 
 #### Directory where the files for various angles are located
-dir = '/Users/hamilton/Qubic/Fibres/ScanAz2019-01-30/'
+#dir = '/Users/hamilton/Qubic/ExternalSource/ScanAz2019-01-30/'
+#dir = '/Users/hamilton/Qubic/ExternalSource/ScanAz2019-01-31/'
+dir = '/Users/hamilton/Qubic/ExternalSource/ScanAz2019-01-31-Long/'
 
 #### Now find the az and el corresponding to each directory and prepare the files to read
 subdirs = glob.glob(dir+'*')
@@ -34,7 +36,7 @@ as2 = np.array(as2)[order]
 subdirs = np.array(subdirs)[order]
 
 ### Analyse one to define the list of pixok
-name = 'ExtSrc'
+name = 'ExtSrc(Auto)'
 fnum = 150
 fff = 0.333
 dc = 0.33
@@ -54,7 +56,7 @@ tt, folded, okfinal, params, err, chi2, ndf = ft.run_asic(fnum,
         reselect_ok=False,
         okfile='ScanAz2019-01-30_OK_Asic2.fits')
 
-#### Now loop on asic1
+#### Now loop on asics
 amps = np.zeros((256,len(az)))
 taus = np.zeros((256,len(az)))
 erramps = np.zeros((256,len(az)))
@@ -62,8 +64,9 @@ errtaus = np.zeros((256,len(az)))
 for i in xrange(len(as1)):
     asic = as1[i]
     tt, folded, okfinal, params, err, chi2, ndf = ft.run_asic(fnum, 0, fff, 
-        dc, asic, 1, name,
+        dc, asic, 1, name, doplot=False,
         initpars=None, lowcut=0.05, highcut=15., okfile='ScanAz2019-01-30_OK_Asic1.fits')
+    params[~okfinal,:] = np.nan
     amps[:128,i] = params[:,3]
     erramps[:128,i] = err[:,3]
     taus[:128,i] = params[:,1]
@@ -71,8 +74,9 @@ for i in xrange(len(as1)):
 
     asic = as2[i]
     tt, folded, okfinal, params, err, chi2, ndf = ft.run_asic(fnum, 0, fff, 
-        dc, asic, 2, name,
+        dc, asic, 2, name, doplot=False,
         initpars=None, lowcut=0.05, highcut=15., okfile='ScanAz2019-01-30_OK_Asic2.fits')
+    params[~okfinal,:] = np.nan
     amps[128:,i] = params[:,3]
     erramps[128:,i] = err[:,3]
     taus[128:,i] = params[:,1]
@@ -81,20 +85,64 @@ for i in xrange(len(as1)):
 
 cutval = 200
 
-allimg = np.zeros((len(as1),17,17))
+allimg = np.zeros((len(as1),17,17)) + np.nan
 for i in xrange(len(as1)):
     allimg[i,:,:] = ft.image_asics(all1=amps[:,i])
     bad = allimg[i,:,:] > cutval
     allimg[i,:,:][bad] = np.nan
     clf()
-    imshow(allimg[i,:,:],vmin=0,vmax=200)
+    imshow(allimg[i,:,:],vmin=0,vmax=75, cmap='viridis')
     colorbar()
     title('$\Delta$az={}'.format(az[i]))
     show()
-    savefig('imgscan_az_{}.png'.format(1000+az[i]))
-    raw_input('Press a key')
+    savefig('imgscan01022019_az_{}.png'.format(1000+az[i]))
+    #raw_input('Press a key')
 
 thepix = 93
 clf()
 plot(az, amps[thepix,:])
 
+
+#### Trying intercalibration from old fiber data (probably worthless as at the time they were superconducting and now they are not)
+
+from pysimulators import FitsArray
+calibration = FitsArray('/Users/hamilton/CMB/Qubic/Fibres/calibration.fits')
+calibration_restrict = FitsArray('/Users/hamilton/CMB/Qubic/Fibres/calibration_restrict.fits')
+
+
+cutval = 200
+
+allimg = np.zeros((len(as1),17,17)) + np.nan
+allimg_c = np.zeros((len(as1),17,17)) + np.nan
+allimg_cr = np.zeros((len(as1),17,17)) + np.nan
+for i in xrange(len(as1)):
+    allimg[i,:,:] = ft.image_asics(all1=amps[:,i])
+    bad = allimg[i,:,:] > cutval
+    allimg[i,:,:][bad] = np.nan
+
+    allimg_c[i,:,:] = ft.image_asics(all1=amps[:,i]*calibration)
+    bad = allimg_c[i,:,:] > cutval
+    allimg_c[i,:,:][bad] = np.nan
+
+    allimg_cr[i,:,:] = ft.image_asics(all1=amps[:,i]*calibration_restrict)
+    bad = allimg_cr[i,:,:] > cutval
+    allimg_cr[i,:,:][bad] = np.nan
+
+    cmap = cm.get_cmap('viridis', 10)
+
+    clf()
+    subplot(1,2,1)
+    imshow(allimg[i,:,:],vmin=0,vmax=75, cmap=cmap)
+    colorbar()
+    title('$\Delta$az={} - Not calibrated'.format(az[i]))
+    subplot(1,2,2)
+    imshow(allimg_c[i,:,:],vmin=0,vmax=75, cmap=cmap)
+    colorbar()
+    title('$\Delta$az={} - Calibrated'.format(az[i]))
+    # subplot(2,2,3)
+    # imshow(allimg_cr[i,:,:],vmin=0,vmax=75, cmap=cmap)
+    # colorbar()
+    # title('$\Delta$az={} - Calibrated (restr)'.format(az[i]))
+    show()
+    savefig('imgscan01022019_calib_az_{}.png'.format(1000+az[i]))
+    #raw_input('Press a key')

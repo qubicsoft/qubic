03b588fc31c3b6aaf7222d963effb6ed0c53669a
Louise Mousset <mousset@apc.in2p3.fr>
2019-02-06 18:01:51 +0100

    lot of work in tracker_source but still not working, detectors are mixed

--- qubic/scripts/Calibration/tracker_source.py
+++ qubic/scripts/Calibration/tracker_source.py
@@ -7,33 +7,31 @@ import healpy as hp
 import qubic
 import fibtools as ft
 
-dictfilename = '/home/louisemousset/QUBIC/MyGitQUBIC/qubic/qubic/scripts/global_source.dict' 
-dictfilename = '/home/james/moussetqubic/qubic/qubic/scripts/global_source.dict'
+dictfilename = '/home/louisemousset/QUBIC/MyGitQUBIC/qubic/qubic/scripts/global_source.dict'
+# dictfilename = '/home/james/moussetqubic/qubic/qubic/scripts/global_source.dict'
 d = qubic.qubicdict.qubicDict()
 d.read_from_file(dictfilename)
 
 # Get sweeping pointing in a fix frame
 p = qubic.get_pointing(d)
 
-
-
-el_angles = np.unique(p.elevation) # all different elevation angles 
-az_angles = np.unique(p.azimuth) # all different azimuth angles 
+el_angles = np.unique(p.elevation)  # all different elevation angles
+az_angles = np.unique(p.azimuth)  # all different azimuth angles
 print('el_angles = ' + str(el_angles))
 print('az_angles = ' + str(az_angles))
 
 plt.figure('pointing')
-plt.subplot(3,1,1)
+plt.subplot(3, 1, 1)
 plt.plot(p.index, p.azimuth, '.')
 plt.xlabel('index pointing')
 plt.ylabel('azimuth angle')
 
-plt.subplot(3,1,2)
+plt.subplot(3, 1, 2)
 plt.plot(p.index, p.elevation, '.')
 plt.xlabel('index pointing')
 plt.ylabel('elevation angle')
 
-plt.subplot(3,1,3)
+plt.subplot(3, 1, 3)
 plt.plot(p.azimuth, p.elevation, '.')
 plt.xlabel('azimuth angle')
 plt.ylabel('elevation angle')
@@ -42,21 +40,23 @@ plt.ylabel('elevation angle')
 az = 50.
 el = 50.
 source_coords = (az, el)
+print('source_coords = {}'.format(source_coords))
 
-npix = 12 * d['nside']**2 # pixel number
+npix = 12 * d['nside'] ** 2  # pixel number
 sky = np.zeros(npix)
-source = hp.pixelfunc.ang2pix(d['nside'], el, az, lonlat=True)
-sky[source] = 1.
-arcToRad = np.pi/(180*60.) #conversion factor
-sky = hp.sphtfunc.smoothing(sky, fwhm=30*arcToRad)
+source_pix = hp.pixelfunc.ang2pix(d['nside'], el, az, lonlat=True)
+sky[source_pix] = 1.
+arcToRad = np.pi / (180 * 60.)  # conversion factor
+sky = hp.sphtfunc.smoothing(sky, fwhm=30 * arcToRad)
 
 hp.gnomview(sky, rot=source_coords)
 
 x0 = np.zeros((d['nf_sub'], npix, 3))
-x0[:,:,0] = sky
+x0[:, :, 0] = sky
+
 
 # Get TOD
-def get_TOD(d, p, x0):
+def get_tod(d, p, x0):
     # Polychromatic instrument model
     q = qubic.QubicMultibandInstrument(d)
 
@@ -64,47 +64,73 @@ def get_TOD(d, p, x0):
     s = qubic.QubicScene(d)
 
     # Number of sub frequencies to build the TOD
-    _, nus_edge_in, _, _, _, _ = qubic.compute_freq(d['filter_nu']/1e9, 
+    _, nus_edge_in, _, _, _, _ = qubic.compute_freq(d['filter_nu'] / 1e9,
                                                     d['filter_relative_bandwidth'], d['nf_sub'])
 
     # Multi-band acquisition operator
     a = qubic.QubicMultibandAcquisition(q, p, s, d, nus_edge_in)
 
-    TOD, _ = a.get_observation(x0, noiseless=True)
-    return TOD
+    tod, _ = a.get_observation(x0, noiseless=True)
 
-TOD = get_TOD(d, p, x0)
-print('TOD.shape = ' + str(TOD.shape))
-nTES = TOD.shape[0] 
-npointings = TOD.shape[1]
+    return q, tod
 
-TOD_mean = np.mean(TOD)
+
+q, tod_fi = get_tod(d, p, x0)
+
+# Take tod of the TD instrument
+nTES = tod_fi.shape[0] / 4
+npointings = tod_fi.shape[1]
+print('nTES = {0} npointings = {1}'.format(nTES, npointings))
+tes_simu = q[0].detector.index[q[0].detector.quadrant == 0]
+
+tod_td = np.take(tod_fi, tes_simu, axis=0)
+
+plt.figure('TOD')
+for i in xrange(248):
+    plt.plot(tod_td[i, :])
+
+tod_mean = np.mean(tod_td)
 
 # Let's insert the 8 thermometers among the TES
-thermos = np.array([3, 35, 67, 99, 131, 163, 195, 227]) # Indices of thermometers
-TOD_thermo = np.zeros((nTES+8,npointings))
+thermos = np.array([3, 35, 67, 99, 131, 163, 195, 227])  # Indices of thermometers
+tod_thermo = np.zeros((nTES + 8, npointings))
 for ptg in xrange(npointings):
-    TOD_thermo[:,ptg] = np.insert(TOD[:,ptg], thermos-np.arange(0,8), 0., axis=None)
-
+    tod_thermo[:, ptg] = np.insert(tod_td[:, ptg], thermos - np.arange(0, 8), 0., axis=None)
 
 # Check thermometers have been inserted at good places
-for i in xrange(nTES+8):
-    if TOD_thermo[i,0]==TOD_mean:
+for i in xrange(nTES + 8):
+    if tod_thermo[i, 0] == tod_mean:
         if i in thermos:
             print(str(i) + ' ok')
         else:
-            print(str(i) + ' bug') 
+            print(str(i) + ' bug')
 
-# Normalization 
-TOD_thermo_mean = TOD_thermo #/ TOD_mean
-#TOD_thermo_mean = np.abs(TOD_thermo - TOD_mean) / TOD_mean
 
-#bug = np.concatenate((TOD_thermo_mean[128:,0], TOD_thermo_mean[0:128, 0]))
+# Use the experimental TES positions
+def mix_tod(tod, thermos):
+
+    tes_exp = ft.image_asics(all1=np.arange(0, 256))
+    tes_exp = np.reshape(tes_exp, 17**2)
+    tes_exp = tes_exp[~np.isnan(tes_exp)]
+    #tes_exp = np.delete(tes_exp, thermos)
+
+
+    tod_mix = np.copy(tod)
+    for i in xrange(256):
+        if i not in thermos:
+            print(i)
+            exp = np.int(tes_exp[i])
+            print(exp)
+            tod_mix[i, :] = tod[exp, :]
+    return tod_mix
+
+tod_thermo_mix = mix_tod(tod_thermo, thermos)
+
 
 plt.figure('focal plane')
-for ptg in xrange(40,75):
-    img = ft.image_asics(all1=TOD_thermo_mean[:,ptg])
-    plt.imshow(np.log(img), interpolation='nearest')
+for ptg in xrange(0, 35):
+    img = ft.image_asics(all1=tod_thermo_mix[:, ptg])
+    plt.imshow(img, interpolation='nearest')
     plt.title('ptg' + str(ptg) + ' el=' + str(p.elevation[ptg]) + ' az=' + str(p.azimuth[ptg]))
     plt.pause(1)
 
@@ -116,19 +142,3 @@ img = ft.image_asics(all1=test)
 plt.figure('TES position')
 plt.imshow(img, interpolation='nearest')
 plt.colorbar()
-
-#Test
-truc = np.concatenate((test[128:], test[0:128]))
-img = ft.image_asics(all1=truc)
-plt.figure('TES position')
-plt.imshow(img, interpolation='nearest')
-plt.colorbar()
-
-plt.figure('focal plane')
-for ptg in xrange(40,75):
-    bug = np.concatenate((TOD_thermo_mean[128:,ptg], TOD_thermo_mean[0:128, ptg]))
-    img = ft.image_asics(all1=bug)
-    plt.imshow(np.log(img), interpolation='nearest')
-    plt.title('ptg' + str(ptg) + ' el=' + str(p.elevation[ptg]) + ' az=' + str(p.azimuth[ptg]))
-    plt.pause(1)
-

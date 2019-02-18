bbb6960fe1a70bf67fb16e99cfd5b4a3f6c2cccd
Louise Mousset <mousset@apc.in2p3.fr>
2019-02-18 14:17:52 +0100

    start to add documentation in functions in input_sky, add a function dust_spectra

--- qubic/scripts/Spectroimagery_paper/input_sky.py
+++ qubic/scripts/Spectroimagery_paper/input_sky.py
@@ -4,40 +4,74 @@ import numpy as np
 
 import qubic
 
+
 def scaling_dust(freq1, freq2, sp_index=1.59):
-    '''
+    """
     Calculate scaling factor for dust contamination
     Frequencies are in GHz
-    '''
+
+    Parameters
+    ----------
+    freq1
+    freq2
+    sp_index
+
+    Returns
+    -------
+
+    """
+
     freq1 = float(freq1)
     freq2 = float(freq2)
     x1 = freq1 / 56.78
     x2 = freq2 / 56.78
-    S1 = x1**2. * np.exp(x1) / (np.exp(x1) - 1)**2.
-    S2 = x2**2. * np.exp(x2) / (np.exp(x2) - 1)**2.
+    s1 = x1 ** 2. * np.exp(x1) / (np.exp(x1) - 1) ** 2.
+    s2 = x2 ** 2. * np.exp(x2) / (np.exp(x2) - 1) ** 2.
     vd = 375.06 / 18. * 19.6
     scaling_factor_dust = (np.exp(freq1 / vd) - 1) / \
                           (np.exp(freq2 / vd) - 1) * \
-                          (freq2 / freq1)**(sp_index + 1)
-    scaling_factor_termo = S1 / S2 * scaling_factor_dust
+                          (freq2 / freq1) ** (sp_index + 1)
+    scaling_factor_termo = s1 / s2 * scaling_factor_dust
     return scaling_factor_termo
 
 
-def dust_spectra(ll, nu):
+def dust_spectra(ll, skypars):
+    """
+
+    Parameters
+    ----------
+    ll
+    skypars
+
+    Returns
+    -------
+
+    """
     fact = (ll * (ll + 1)) / (2 * np.pi)
-    coef = 1.39e-2
+    coef = skypars['dust_coeff']
     spectra_dust = [np.zeros(len(ll)),
-                  coef * (ll / 80.)**(-0.42) / (fact * 0.52),
-                  coef * (ll / 80.)**(-0.42) / fact,
-                  np.zeros(len(ll))]
-    sc_dust = scaling_dust(150, nu)
-    return fact * sc_dust * spectra_dust
+                    coef * (ll / 80.) ** (-0.42) / (fact * 0.52),
+                    coef * (ll / 80.) ** (-0.42) / fact,
+                    np.zeros(len(ll))]
+    return spectra_dust
 
 
 def cmb_plus_dust(cmb, dust, Nbsubbands, sub_nus, kind='IQU'):
-    '''
+    """
     Sum up clean CMB map with dust using proper scaling coefficients
-    '''
+
+    Parameters
+    ----------
+    cmb
+    dust
+    Nbsubbands
+    sub_nus
+    kind
+
+    Returns
+    -------
+
+    """
     Nbpixels = cmb.shape[0]
     nstokes = len(kind)  # Number of stokes parameters used in the simu
     x0 = np.zeros((Nbsubbands, Nbpixels, 3))
@@ -52,6 +86,18 @@ def cmb_plus_dust(cmb, dust, Nbsubbands, sub_nus, kind='IQU'):
 
 
 def create_input_sky(d, skypars):
+    """
+
+    Parameters
+    ----------
+    d : file .dict
+    skypars : dictionary
+        Contains the dust coeff and r
+
+    Returns
+    -------
+    x0 : a sky with 3 components I, Q, U
+    """
     Nf = int(d['nf_sub'])
     band = d['filter_nu'] / 1e9
     filter_relative_bandwidth = d['filter_relative_bandwidth']
@@ -63,13 +109,14 @@ def create_input_sky(d, skypars):
         sp = qubic.read_spectra(skypars['r'])
         cmb = np.array(hp.synfast(sp, d['nside'], new=True, pixwin=True, verbose=False)).T
         # Generate the dust map
-        coef = skypars['dust_coeff']
         ell = np.arange(1, 3 * d['nside'])
-        fact = (ell * (ell + 1)) / (2 * np.pi)
-        spectra_dust = [np.zeros(len(ell)),
-                        coef * (ell / 80.) ** (-0.42) / (fact * 0.52),
-                        coef * (ell / 80.) ** (-0.42) / fact,
-                        np.zeros(len(ell))]
+        spectra_dust = dust_spectra(ell, skypars)
+        # coef = skypars['dust_coeff']
+        # fact = (ell * (ell + 1)) / (2 * np.pi)
+        # spectra_dust = [np.zeros(len(ell)),
+        #                 coef * (ell / 80.) ** (-0.42) / (fact * 0.52),
+        #                 coef * (ell / 80.) ** (-0.42) / fact,
+        #                 np.zeros(len(ell))]
         dust = np.array(hp.synfast(spectra_dust, d['nside'], new=True, pixwin=True, verbose=False)).T
 
         # Combine CMB and dust. As output we have N 3-component maps of sky.

f8b2b5c915b2b48e6eec7b5a88962b821092d137
Louise Mousset <mousset@apc.in2p3.fr>
2019-03-04 09:46:15 +0100

    small modifications

--- qubic/scripts/Calibration/integrating_sphere.py
+++ qubic/scripts/Calibration/integrating_sphere.py
@@ -61,46 +61,51 @@ def make_amp(dir, name='Integrating_sphere', fnum=150, fff=0.15, dc=0.33):
 # Ref, data 2019-02-14
 dir = basedir + '2019-02-14/2019-02-14_16.53.38__Test_int_sphere150Ghz_150mHz'
 amps = make_amp(dir)
-img_ref = ft.image_asics(all1=amps / np.nanmean(amps))
+# img_ref = ft.image_asics(all1=amps / np.nanmean(amps))
 
-amps_all = []
-amps_all.append(amps)
+amps_all = [amps]
 
 # Test with different positions
-for pos in xrange(1, 6):
+allimg = np.zeros((4, 17, 17))  # contains datas from 2019-02-15
+for pos in xrange(1, 5):
     dir = basedir + '2019-02-15/*_pos{}'.format(pos)
     amps = make_amp(dir, fff=0.333)
     amps_all.append(amps)
+    img = ft.image_asics(all1=amps_all[pos])
+    allimg[pos-1, :, :] = img
+
 
 # Plot for different positions compared to the ref
-figure('Integration Sphere, divided by 2019-02-14 ')
-for pos in xrange(5):
-    img = ft.image_asics(all1=amps_all[pos] / np.nanmean(amps_all[pos]))
-    subplot(2, 3, pos + 1)
-    imshow(img / img_ref, cmap='viridis', vmin=0, vmax=3, interpolation='nearest')
+figure('Integration Sphere, divided the mean of 2019-02-15 measurements ')
+for pos in xrange(4):
+    # img = ft.image_asics(all1=amps_all[pos] / np.nanmean(amps_all[pos]))
+    subplot(2, 2, pos + 1)
+    imshow(allimg[pos] / np.nanmean(allimg, axis=0), cmap='viridis', vmin=0, vmax=3, interpolation='nearest')
+
+    # imshow(img / img_ref, cmap='viridis', vmin=0, vmax=3, interpolation='nearest')
     # imshow(img / np.nanmean(img), cmap='viridis', vmin=0, vmax=3, interpolation='nearest')
     colorbar()
-    if pos == 0:
-        title('Int Sphere Ref 2019-02-14')
-    else:
-        title('Int Sphere 2019-02-15 pos{}'.format(pos))
+    # if pos == 0:
+    #     title('Int Sphere Ref 2019-02-14')
+    # else:
+    title('Int Sphere 2019-02-15 pos{}'.format(pos+1))
 
-savefig(basedir + 'int_sphere_pos1-4_divided_ref')
+# savefig(basedir + 'int_sphere_pos1-4_divided_ref')
 
 # Try intercalibration
-intercal = img / img[16, 0]
-
-allimg = np.array(FitsArray('allimg_scan_az.fits'))
-az = np.array(FitsArray('az_scan_az.fits'))
-
-for i in xrange(len(az)):
-    clf()
-    subplot(1, 2, 1)
-    imshow(allimg[i, :, :], cmap='viridis', vmin=0, vmax=1000)
-    title(az[i])
-    subplot(1, 2, 2)
-    imshow(allimg[i, :, :] / intercal, cmap='viridis', vmin=0, vmax=1000)
-    title('Intercalibrated')
-    show()
-    savefig('scan_az_int_sphere_{}.png'.format(1100 + az[i]))
-    # raw_input('press a key')
+# intercal = img / img[16, 0]
+#
+# allimg = np.array(FitsArray('allimg_scan_az.fits'))
+# az = np.array(FitsArray('az_scan_az.fits'))
+#
+# for i in xrange(len(az)):
+#     clf()
+#     subplot(1, 2, 1)
+#     imshow(allimg[i, :, :], cmap='viridis', vmin=0, vmax=1000)
+#     title(az[i])
+#     subplot(1, 2, 2)
+#     imshow(allimg[i, :, :] / intercal, cmap='viridis', vmin=0, vmax=1000)
+#     title('Intercalibrated')
+#     show()
+#     savefig('scan_az_int_sphere_{}.png'.format(1100 + az[i]))
+#     # raw_input('press a key')

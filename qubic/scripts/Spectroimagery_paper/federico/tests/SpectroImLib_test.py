0178fcd4a63b093adb95ac076c4d2311be641db3
fincardona <incardona.federico@gmail.com>
2019-04-03 17:34:12 +0200

    Add new tests

--- qubic/scripts/Spectroimagery_paper/federico/tests/SpectroImLib_test.py
+++ qubic/scripts/Spectroimagery_paper/federico/tests/SpectroImLib_test.py
@@ -86,7 +86,68 @@ class TestSpectroImLib(ut.TestCase):
         maps_p_all = sky_p_all.get_sky_map()
 
         self.assertTrue(np.allclose(maps_p_all[4], maps_p[0]))
-        
+
+        instrument_nless_pless = pysm.Instrument({
+            'nside': d['nside'],
+            'frequencies' : sky_p.planck_central_nus[4:5], # GHz
+            'use_smoothing' : False,
+            'beams' : None, # arcmin 
+            'add_noise' : False,
+            'noise_seed' : None,
+            'sens_I': None, 
+            'sens_P': None, 
+            'use_bandpass' : True, 
+            'channel_names' : sky_p.planck_channels_names[4:5],
+            'channels' : sky_p.planck_channels[4:5],
+            'output_units' : 'uK_RJ',
+            'output_directory' : "./",
+            'output_prefix' : "planck_sky_pless_nless",
+            'pixel_indices' : None})
+        sky_p_nless_pless = si.sky(sky_config, d, instrument_nless_pless)
+        maps_p_nless_pless = sky_p_nless_pless.get_sky_map()
+        list_nless_pless, _ = sky_p_nless_pless.read_sky_map()
+        self.assertTrue(
+            list_nless_pless ==
+            ['planck_sky_pless_nless_bandpass_143_GHz_total_nside0128.fits'])
+        disintegrated_maps = np.rollaxis(sky_p.sky.signal()(
+            nu=sky_p.planck_channels[4:5][0][0]), 2, 1)
+        integrated_maps = np.mean(disintegrated_maps, axis=0) 
+#        self.assertTrue(np.allclose(maps_p_nless_pless, integrated_maps))
+
+        instrument_noisy = pysm.Instrument({
+            'nside': d['nside'],
+            'frequencies' : sky_p.planck_central_nus[4:5], # GHz
+            'use_smoothing' : False,
+            'beams' : None, # arcmin 
+            'add_noise' : True,
+            'noise_seed' : 0,
+            'sens_I': sky_p.get_planck_sensitivity("I")[4:5], 
+            'sens_P': sky_p.get_planck_sensitivity("Q")[4:5], 
+            'use_bandpass' : False, 
+            'channel_names' : sky_p.planck_channels_names[4:5],
+            'channels' : None,
+            'output_units' : 'uK_RJ',
+            'output_directory' : "./",
+            'output_prefix' : "planck_sky_noisy",
+            'pixel_indices' : None})
+        sky_p_noisy = si.sky(sky_config, d, instrument_noisy)
+        maps_p_noisy = sky_p_noisy.get_sky_map()
+        list_noisy, _ = sky_p_noisy.read_sky_map()
+        self.assertTrue(
+            list_noisy ==
+            ['planck_sky_noisy_nu0143p00GHz_total_nside0128.fits'])
+        noise_map = maps_p_noisy[0] - sky_p.sky.signal()(
+            nu=sky_p.planck_central_nus[4:5]).T
+        noise_map_produced = hp.read_map(
+            'planck_sky_noisy_nu0143p00GHz_noise_nside0128.fits', field=(0,1,2)).T
+        self.assertTrue(np.allclose(noise_map, noise_map_produced, atol=9e-3))
+#        self.assertTrue(np.std(noise_map, axis=0)[0] ==
+#                        sky_p.get_planck_sensitivity("I")[4:5])
+#        self.assertTrue(np.std(noise_map, axis=0)[1] ==
+#                        sky_p.get_planck_sensitivity("P")[4:5])
+#        self.assertTrue(np.std(noise_map, axis=0)[2] ==
+#                        sky_p.get_planck_sensitivity("P")[4:5])
+            
         # instrument_mono = pysm.Instrument({
         #     'nside': d['nside'],
         #     'frequencies' : sky_p.planck_central_nus[4:5], # GHz
@@ -103,8 +164,7 @@ class TestSpectroImLib(ut.TestCase):
         #     'output_directory' : "./",
         #     'output_prefix' : "planck_sky_mono",
         #     'pixel_indices' : None})
-        # sky_p_mono = si.sky(sky_config, d, instrument_mono, "./",
-        #                     "planck_sky_mono")
+        # sky_p_mono = si.sky(sky_config, d, instrument_mono)
         # maps_p_mono = sky_p_mono.get_sky_map()
         # list_mono, _ = sky_p_mono.read_sky_map()
         # self.assertTrue(list_mono ==
@@ -127,8 +187,7 @@ class TestSpectroImLib(ut.TestCase):
         #     'output_directory' : "./",
         #     'output_prefix' : "planck_sky_nless_mono",
         #     'pixel_indices' : None})
-        # sky_p_mono_nless = si.sky(sky_config, d, instrument_mono_nless,
-        #                            "./", "planck_sky_nless_mono")
+        # sky_p_mono_nless = si.sky(sky_config, d, instrument_mono_nless)
         # maps_p_mono_nless = sky_p_mono_nless.get_sky_map()
         # list_mono_nless, _ = sky_p_mono_nless.read_sky_map()
         # self.assertTrue(

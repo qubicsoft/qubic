522e20192283b600813b9d49462b32f7e9b976d5
Louise Mousset <mousset@apc.in2p3.fr>
2019-05-15 12:21:28 +0200

    correct comments and put a list of reconstructed subband instead of noutmin, noutmax

--- qubic/scripts/Spectroimagery_paper/make_maps_clean.py
+++ qubic/scripts/Spectroimagery_paper/make_maps_clean.py
@@ -28,7 +28,7 @@ d = qubic.qubicdict.qubicDict()
 d.read_from_file(dictfilename)
 
 # Print dictionary and others parameters
-# Save a file with al parameters
+# Save a file with all parameters
 tem = sys.stdout
 sys.stdout = f = open(out_dir + name + '.txt', 'wt')
 
@@ -37,8 +37,7 @@ print('Dictionnary File: ' + dictfilename)
 for k in d.keys():
     print(k, d[k])
 
-print('Minimum Number of Sub Frequencies: {}'.format(noutmin))
-print('Maximum Number of Sub Frequencies: {}'.format(noutmax))
+print('Numbers of reconstructed subbands: {}'.format(nrecon))
 
 sys.stdout = tem
 f.close()
@@ -66,7 +65,7 @@ p = qubic.get_pointing(d)
 TOD, maps_convolved = si.create_TOD(d, p, x0)
 
 # ==== Reconstruction ====
-for nf_sub_rec in nrecon: # np.arange(noutmin, noutmax + 1):
+for nf_sub_rec in nrecon:
     print(nf_sub_rec)
     print('-------------------------- Map-Making on {} sub-map(s)'.format(nf_sub_rec))
     maps_recon, cov, nus, nus_edge, maps_convolved = si.reconstruct_maps(TOD, d, p, nf_sub_rec, x0=x0)
@@ -80,8 +79,6 @@ for nf_sub_rec in nrecon: # np.arange(noutmin, noutmax + 1):
     maps_recon[:, unseen, :] = hp.UNSEEN
     print('************************** Map-Making on {} sub-map(s)Done'.format(nf_sub_rec))
 
-    # hdu_convolved = fits.PrimaryHDU()
-    # hdu_recon.data =
 
     # FitsArray(nus_edge).save(name + '_nf{0}_ptg{1}'.format(nf_sub_rec, ptg) + '_nus_edges.fits')
     # FitsArray(nus).save(name + '_nf{0}_ptg{1}'.format(nf_sub_rec, ptg)+ '_nus.fits')

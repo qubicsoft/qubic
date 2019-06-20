ab4ba170e87a69f4db52c8fc4a2771b5196e79ab
Louise Mousset <mousset@apc.in2p3.fr>
2019-06-20 09:48:26 +0200

    add residual computaion

--- qubic/scripts/Spectroimagery_paper/analyse_maps.py
+++ qubic/scripts/Spectroimagery_paper/analyse_maps.py
@@ -16,11 +16,11 @@ center = equ2gal(0., -57.)
 
 # ================= Get the simulation files ================
 # repository where the .fits was saved
-date = '20190617'
+date = '20190619'
 rep_simu = './TEST/{}/'.format(date)
 
 # Simulation name
-name = 'minitest'
+name = 'firstdemo'
 
 # Dictionary saved during the simulation
 d = qubic.qubicdict.qubicDict()
@@ -35,7 +35,7 @@ for fits in glob.glob(rep_simu + date + '_' + name + '*.fits'):
 # Number of subbands used during the simulation
 nf_recon = d['nf_recon']
 
-# ================= Get the maps ================
+# ================= Get maps ================
 # Get seen map (observed pixels)
 seen_map = rmc.get_seenmap(fits_files[0])
 
@@ -43,7 +43,8 @@ seen_map = rmc.get_seenmap(fits_files[0])
 npix = len(seen_map)
 ns = d['nside']
 
-# Get full maps
+
+# Get one full maps
 maps_recon, maps_convo, maps_diff = rmc.get_maps(fits_files[1])
 print('Getting maps with shape : {}'.format(maps_recon.shape))
 
@@ -58,26 +59,32 @@ for i in xrange(3):
     hp.gnomview(maps_diff[isub, :, i], rot=center, reso=9, sub=(3, 3, 6 + i + 1),
                 title='residus ' + stokes[i])
 
-# Get only patches to save memory
+# Get one patch
 maps_recon_cut, maps_convo_cut, maps_diff_cut = rmc.get_patch(fits_files[1], seen_map)
 print('Getting patches with shape : {}'.format(maps_recon_cut.shape))
 
+# Get all patches (all noise realisations)
+all_fits, all_patch_recon, all_patch_conv, all_patch_diff = rmc.get_patch_many_files(
+    rep_simu, date + '_' + name + '*nfrecon2_noiselessFalse*.fits')
+print('Getting all patch realizations with shape : {}'.format(all_patch_recon.shape))
 
 # ================== Look at residuals ===============
+residuals = all_patch_recon - np.mean(all_patch_recon, axis=0)
 
-# Histogram of the residuals (first subband)
+# Histogram of the residuals (first real, first subband)
 plt.clf()
 for i in xrange(3):
     plt.subplot(1, 3, i + 1)
-    plt.hist(np.ravel(maps_diff_cut[0, :, i]), range=[-5, 5], bins=100)
+    plt.hist(np.ravel(residuals[0, 0, :, i]), range=[-5, 5], bins=100)
     plt.title(stokes[i])
+
+# ================= Make zones ============
 
 # ================= Correlations matrices=======================
 # Correlation between pixels
+cov_pix, corr_pix = amc.get_covcorr_between_pix(residuals, verbose=True)
 
 # Correlations between subbands and I, Q, U
-
-
 
 # ================= Noise Evolution as a function of the subband number=======================
 # This part should be rewritten (old)

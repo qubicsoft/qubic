7d41f68d67b32b119c75969106f30ddb2c935cbb
Louise Mousset <mousset@apc.in2p3.fr>
2019-07-09 11:02:37 +0200

    simplify import fits files

--- qubic/scripts/Spectroimagery_paper/analyse_maps.py
+++ qubic/scripts/Spectroimagery_paper/analyse_maps.py
@@ -11,57 +11,51 @@ from qubic import equ2gal
 
 stokes = ['I', 'Q', 'U']
 
-
 # ================= Get the simulation files ================
 # repository where the .fits was saved
-#date = '20190619'
-date = '20190620'
-rep_simu = './TEST/{}/'.format(date)
+date = '20190704'
+# rep_simu = './TEST/{}/'.format(date)
+rep_simu = '/home/louisemousset/QUBIC/Qubic_work/SpectroImagerie/SimuLouise/Noise_MCMC_201907/' + date + '/'
 
 # Simulation name
-#name = 'firsttest'
-name = 'lowtol'
-
+name = 'try50reals'
 
 # Dictionary saved during the simulation
 d = qubic.qubicdict.qubicDict()
 d.read_from_file(rep_simu + date + '_' + name + '.dict')
-# Coordinates of the zone observed in the sky
 
+# Coordinates of the zone observed in the sky
 center = equ2gal(d['RA_center'], d['DEC_center'])
 
 # Get fits files names in a list
-fits_files = []
-for fits in glob.glob(rep_simu + date + '_' + name + '*.fits'):
-    fits_files.append(fits)
-    print(fits)
+fits_noise = glob.glob(rep_simu + date + '_' + name + '*noiselessFalse*.fits')
+fits_noiseless = glob.glob(rep_simu + date + '_' + name + '*noiselessTrue*.fits')
 
 # Number of subbands used during the simulation
 nf_recon = d['nf_recon']
 
 # ================= Get maps ================
 # Get seen map (observed pixels)
-seen_map = rmc.get_seenmap(fits_files[0])
+seen_map = rmc.get_seenmap(fits_noise[0])
 
 # Number of pixels and nside
 npix = len(seen_map)
 ns = d['nside']
 
-
 # Get one full maps
-maps_recon, maps_convo, maps_diff = rmc.get_maps(fits_files[1])
+maps_recon, maps_convo, maps_diff = rmc.get_maps(fits_noiseless[0])
 print('Getting maps with shape : {}'.format(maps_recon.shape))
 
 # Look at the maps
 isub = 0
-plt.figure('Maps in subband {}'.format(isub))
+plt.figure('Noiseless maps')
 for i in xrange(3):
     hp.gnomview(maps_convo[isub, :, i], rot=center, reso=9, sub=(3, 3, i + 1),
-                title='conv ' + stokes[i])
+                title='conv ' + stokes[i] + ' subband {}/{}'.format(isub+1, nf_recon[0]))
     hp.gnomview(maps_recon[isub, :, i], rot=center, reso=9, sub=(3, 3, 3 + i + 1),
-                title='recon ' + stokes[i])
+                title='recon ' + stokes[i] + ' subband {}/{}'.format(isub+1, nf_recon[0]))
     hp.gnomview(maps_diff[isub, :, i], rot=center, reso=9, sub=(3, 3, 6 + i + 1),
-                title='residus ' + stokes[i])
+                title='diff ' + stokes[i] + ' subband {}/{}'.format(isub+1, nf_recon[0]))
 
 # Get one patch
 maps_recon_cut, maps_convo_cut, maps_diff_cut = rmc.get_patch(fits_files[1], seen_map)
@@ -91,12 +85,12 @@ cov_pix, corr_pix = amc.get_covcorr_between_pix(residuals, verbose=True)
 istk = 0
 isub = 0
 plt.subplot(121)
-plt.title('Covariance matrix between pixels, nsub = {}, istokes = {}'.format(isub,stokes[istk]))
+plt.title('Covariance matrix between pixels, nsub = {}, istokes = {}'.format(isub, stokes[istk]))
 plt.imshow(cov_pix[isub, istk, :, :])
 plt.colorbar()
 
 plt.subplot(122)
-plt.title('Correlation matrix between pixels, nsub = {}, istokes = {}'.format(isub,stokes[istk]))
+plt.title('Correlation matrix between pixels, nsub = {}, istokes = {}'.format(isub, stokes[istk]))
 plt.imshow(corr_pix[isub, istk, :, :])
 plt.colorbar()
 
@@ -105,10 +99,10 @@ nsub = corr_pix.shape[0]
 distance = np.empty((nsub, 3))
 for isub in range(nsub):
     for istk in range(3):
-        distance[isub, istk] = amc.distance_square(corr_pix[isub,istk,:,:])
+        distance[isub, istk] = amc.distance_square(corr_pix[isub, istk, :, :])
 
 # Correlations between subbands and I, Q, U
-amc.get_covcorr_patch(residuals, doplot = True, bins = 60)
+amc.get_covcorr_patch(residuals, doplot=True, bins=60)
 
 # ================= Noise Evolution as a function of the subband number=======================
 # This part should be rewritten (old)
@@ -124,5 +118,3 @@ for i in xrange(3):
 plt.xlabel('Number of sub-frequencies')
 plt.ylabel('Relative maps RMS')
 plt.legend()
-
-

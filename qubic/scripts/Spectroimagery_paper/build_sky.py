aba379e17a595d3c52d87d19a45577790795d5f6
Louise Mousset <mousset@apc.in2p3.fr>
2019-08-13 12:08:02 -0300

    simplify imports and put nf_sub by hand

--- qubic/scripts/Spectroimagery_paper/build_sky.py
+++ qubic/scripts/Spectroimagery_paper/build_sky.py
@@ -1,15 +1,10 @@
 from __future__ import division
-import sys
-import time
 
-import healpy as hp
 import numpy as np
 
 from pysm.nominal import models
 
 import qubic
-
-import ReadMC
 import SpectroImLib as si
 import os
 from pysimulators import FitsArray
@@ -19,25 +14,23 @@ dictfilename = './spectroimaging.dict'
 d = qubic.qubicdict.qubicDict()
 d.read_from_file(dictfilename)
 
-nf_sub = d['nf_sub']
+nf_sub = np.arange(1, 15)
 dirc = './maps/'
 
 try:
-	os.makedirs(dirc)
+    os.makedirs(dirc)
 except:
-	pass
+    pass
 
-for  nf in nf_sub:
-	sky_config = {'dust': models('d1', d['nside']),'cmb': models('c1', d['nside'])}
-	
-	d['nf_sub'] = nf
-	
-	Qubic_sky = si.Qubic_sky(sky_config, d)
-	x0 = Qubic_sky.get_simple_sky_map()
-	dirc2 = dirc+'nf_sub={}/'.format(nf) 
-	try:
-		os.makedirs(dirc2)
-	except:
-		pass
-	FitsArray(x0).save(dirc2+'nf_sub={}.fits'.format(nf))
+for nf in nf_sub:
+    print(nf)
+    sky_config = {'dust': models('d1', d['nside']), 'cmb': models('c1', d['nside'])}
 
+    Qubic_sky = si.Qubic_sky(sky_config, d)
+    x0 = Qubic_sky.get_simple_sky_map()
+    dirc2 = dirc + 'nf_sub={}/'.format(nf)
+    try:
+        os.makedirs(dirc2)
+    except:
+        pass
+    FitsArray(x0).save(dirc2 + 'nf_sub={}.fits'.format(nf))

92036e909d0cc1ce8bff39fc0557f43badc6b272
Louise Mousset <mousset@apc.in2p3.fr>
2019-09-16 11:13:33 -0300

    remove old functions that do not work anymore wit the new qubicpack version

--- qubic/scripts/Calibration/selfcal_lib.py
+++ qubic/scripts/Calibration/selfcal_lib.py
@@ -460,6 +460,7 @@ class SelfCalibration:
             Power on the total focal plane for each pointing.
 
         Returns
+        ----------
         quart_fp : array of shape (17, 17)
             The power on a quarter of the focal plane
 
@@ -515,52 +516,3 @@ class SelfCalibration:
         power = np.fliplr(power)  # There is a symmetry bug, need to flip it
 
         return power
-
-
-# ============= JC functions old
-def tes2imgpix(tesnum, extra_args=None):
-    if extra_args is None:
-        a1 = qp()
-        a1.assign_asic(1)
-        a2 = qp()
-        a2.assign_asic(2)
-    else:
-        a1 = extra_args[0]
-        a2 = extra_args[1]
-
-    ij = np.zeros((len(tesnum), 2))
-    for i in range(len(tesnum)):
-        if i < 128:
-            pixnum = a1.tes2pix(tesnum[i])
-            ww = np.where(a1.pix_grid == pixnum)
-        else:
-            pixnum = a2.tes2pix(tesnum[i] - 128)
-            ww = np.where(a2.pix_grid == pixnum)
-        if len(ww[0]) > 0:
-            ij[i, :] = ww
-        else:
-            ij[i, :] = [17, 17]
-    return ij
-
-
-def fringe_focalplane(x, pars, extra_args=None):
-    baseline = pars[0]
-    alpha = pars[1]
-    phase = pars[2]
-    amplitude = pars[3]
-    nu = 150e9
-    lam = 3e8 / nu
-    f = 300e-3  # Focal Length in mm
-    freq_fringe = baseline / lam
-    TESsize = 3.e-3
-
-    ijtes = tes2imgpix(np.arange(256) + 1, extra_args=extra_args)
-
-    fringe = amplitude * np.cos(2. * np.pi * freq_fringe * (
-            ijtes[:, 0] * np.cos(alpha * np.pi / 180) + ijtes[:, 1] * np.sin(
-        alpha * np.pi / 180)) * TESsize / f + phase * np.pi / 180)
-    thermos = [4 - 1, 36 - 1, 68 - 1, 100 - 1, 4 - 1 + 128, 36 - 1 + 128, 68 - 1 + 128, 100 - 1 + 128]
-    fringe[thermos] = 0
-    mask = x > 0
-    fringe[~mask] = 0
-    return fringe

88fe723e68352b31e7248540f605c463cdc84800
Jean-Christophe Hamilton <hamilton@apcdhcp46.in2p3.fr>
2019-10-07 15:31:58 +0200

    added encode='latin1' to pickle load for compatibility with python3

--- qubic/sb_fitting.py
+++ qubic/sb_fitting.py
@@ -82,7 +82,7 @@ def get_flatmap(TESNum, directory, azmin=None, azmax=None, elmin=None, elmax=Non
 	else:
 		### Read fitted synthesized beam
 		thefile = open(fitted_directory+'/fit-TES{}.pk'.format(TESNum), 'rb')
-		fitpars = pickle.load(thefile)
+		fitpars = pickle.load(thefile, encoding='latin1')
 		thefile.close
 		sbfitmodel = SbModelIndepPeaks(nrings=2, common_fwhm=True, no_xy_shift=False,distortion=False)
 		x =  np.meshgrid(az*np.cos(np.radians(50)), np.flip(el))

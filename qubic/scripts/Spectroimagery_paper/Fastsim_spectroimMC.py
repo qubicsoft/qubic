ee228ab723be7dcb32f08eedd255da23f0345ab1
Louise Mousset <lmousset@cca001.in2p3.fr>
2020-06-28 01:02:43 +0200

    correct small bugs

--- qubic/scripts/Spectroimagery_paper/Fastsim_spectroimMC.py
+++ qubic/scripts/Spectroimagery_paper/Fastsim_spectroimMC.py
@@ -55,9 +55,12 @@ all_nfrecon = [1, 2, 3, 4, 5, 8]
 nfrecon = int(sys.argv[2])
 if nfrecon not in all_nfrecon:
     raise ValueError('Wrong number of subbands !')
+print('\n nfrecon:', nfrecon)
 
 # Get maps
 maps, coverage, seenmap = get_maps_from_louise(dirmaps, nfrecon, d['nside'])
+# np.save(rep_save + '/seenmap_nfrecon{}'.format(nfrecon)+ '.npy', seenmap)
+# np.save(rep_save + '/coverage_nfrecon{}'.format(nfrecon)+ '.npy', coverage)
 
 # Compute covariance matrices
 cI, cQ, cU, _, _ = qss.get_cov_nunu(maps, coverage, QUsep=True)
@@ -79,7 +82,9 @@ for isub in range(nfrecon):
 # Make many maps realisations with clnoise=None
 nreals = int(sys.argv[3])
 signoise = 75
-npix = 12 * d['nside'] ** 2
+nside = d['nside']
+print('nside:', nside)
+npix = 12 * nside ** 2
 maps_clnoiseNone = np.zeros((nreals, nfrecon, npix, 3))
 
 rnd_name = qss.random_string(10)
@@ -91,8 +96,10 @@ for i in range(nreals):
                                                       clnoise=None,
                                                       sub_bands_cov=[cI, cQ, cU],
                                                       verbose=False)
-with open(rep_save + 'maps_clnoiseNone_nfrecon{}_nreals{}' + rnd_name + '.pkl'.format(nfrecon, nreals), 'wb') as f:
-    pickle.dump(maps_clnoiseNone, f)
+np.save(rep_save + '/patch_clnoiseNone_nfrecon{}_nreals{}_'.format(nfrecon, nreals) + rnd_name + '.npy',
+        maps_clnoiseNone[:, :, seenmap, :])
+# with open(rep_save + '/maps_clnoiseNone_nfrecon{}_nreals{}_'.format(nfrecon, nreals) + rnd_name + '.pkl', 'wb') as f:
+#     pickle.dump(maps_clnoiseNone, f)
 
 # Make many maps realisations with clnoise=clth
 # Spatial correlation for noise
@@ -109,71 +116,73 @@ for i in range(nreals):
                                                clnoise=clth,
                                                sub_bands_cov=[cI, cQ, cU],
                                                verbose=False)
-with open(rep_save + 'maps_clth_nfrecon{}_nreals{}' + rnd_name + '.pkl'.format(nfrecon, nreals), 'wb') as f:
-    pickle.dump(maps_clth, f)
+np.save(rep_save + '/patch_clth_nfrecon{}_nreals{}_'.format(nfrecon, nreals) + rnd_name + '.npy',
+        maps_clth[:, :, seenmap, :])
+# with open(rep_save + '/maps_clth_nfrecon{}_nreals{}_'.format(nfrecon, nreals) + rnd_name + '.pkl', 'wb') as f:
+#     pickle.dump(maps_clth, f)
 
-# ================== Power spectrum =============================
-print('\n =============== Starting power spectrum ================')
-# Create a Namaster object
-lmin = 40
-lmax = 2 * d['nside'] - 1
-delta_ell = 30
-
-mask = np.zeros(12 * d['nside'] ** 2)
-mask[seenmap] = 1
-Namaster = nam.Namaster(mask, lmin=lmin, lmax=lmax, delta_ell=delta_ell)
-mask_apo = Namaster.get_apodized_mask()
-
-ell_binned, b = Namaster.get_binning(d['nside'])
-nbins = len(ell_binned)
-print('lmin:', lmin)
-print('lmax:', lmax)
-print('delta_ell:', delta_ell)
-print('nbins:', nbins)
-print('ell binned:', ell_binned)
-
-# Auto spectrum
-print('\n =============== Auto spectrum starting================')
-w = None
-cells_auto = np.zeros((nreals, nfrecon, nbins, 4))
-for r in range(nreals):
-    for isub in range(nfrecon):
-        maps = maps_clth[r, isub, :, :]
-        leff, cells_auto[isub, :, :], w = Namaster.get_spectra(maps.T,
-                                                               mask_apo,
-                                                               w=w,
-                                                               purify_e=True,
-                                                               purify_b=False,
-                                                               beam_correction=None,
-                                                               pixwin_correction=True)
-with open(rep_save + 'auto_spectrum_nfrecon{}_nreals{}' + rnd_name + '.pkl'.format(nfrecon, nreals), 'wb') as f:
-    pickle.dump(cells_auto, f)
-
-# Cross spectrum
-print('\n =============== Cross spectrum starting ================')
-ncross = nreals // 2
-print('ncross:', ncross)
-cells_cross = np.zeros((ncross, nfrecon, nbins, 4))
-
-# Get spectra
-w = None
-for isub in range(nfrecon):
-    print('isub:', isub)
-    cross = 0
-    for c1 in range(0, nreals - 1, 2):  # do not mix pairs to avoid correlation
-        c2 = c1 + 1
-        c = (c1, c2)
-        print(c)
-        map1 = maps_clth[c[0], isub, :, :]
-        map2 = maps_clth[c[1], isub, :, :]
-        leff, cells_cross[cross, isub, :, :], w = Namaster.get_spectra(map1.T,
-                                                                       mask_apo,
-                                                                       map2.T,
-                                                                       w=w,
-                                                                       purify_e=True,
-                                                                       purify_b=False,
-                                                                       beam_correction=None,
-                                                                       pixwin_correction=True)
-        cross += 1
-with open(rep_save + 'cross_spectrum_nfrecon{}_nreals{}' + rnd_name + '.pkl'.format(nfrecon, nreals), 'wb') as f:
-    pickle.dump(cells_cross, f)
+# # ================== Power spectrum =============================
+# print('\n =============== Starting power spectrum ================')
+# # Create a Namaster object
+# lmin = 40
+# lmax = 2 * d['nside'] - 1
+# delta_ell = 30
+#
+# mask = np.zeros(12 * d['nside'] ** 2)
+# mask[seenmap] = 1
+# Namaster = nam.Namaster(mask, lmin=lmin, lmax=lmax, delta_ell=delta_ell)
+# mask_apo = Namaster.get_apodized_mask()
+#
+# ell_binned, b = Namaster.get_binning(d['nside'])
+# nbins = len(ell_binned)
+# print('lmin:', lmin)
+# print('lmax:', lmax)
+# print('delta_ell:', delta_ell)
+# print('nbins:', nbins)
+# print('ell binned:', ell_binned)
+#
+# # Auto spectrum
+# print('\n =============== Auto spectrum starting================')
+# w = None
+# cells_auto = np.zeros((nreals, nfrecon, nbins, 4))
+# for r in range(nreals):
+#     for isub in range(nfrecon):
+#         maps = maps_clth[r, isub, :, :]
+#         leff, cells_auto[r, isub, :, :], w = Namaster.get_spectra(maps.T,
+#                                                                   mask_apo,
+#                                                                   w=w,
+#                                                                   purify_e=True,
+#                                                                   purify_b=False,
+#                                                                   beam_correction=None,
+#                                                                   pixwin_correction=True)
+# with open(rep_save + '/auto_spectrum_nfrecon{}_nreals{}_'.format(nfrecon, nreals) + rnd_name + '.pkl', 'wb') as f:
+#     pickle.dump(cells_auto, f)
+#
+# # Cross spectrum
+# print('\n =============== Cross spectrum starting ================')
+# ncross = nreals // 2
+# print('ncross:', ncross)
+# cells_cross = np.zeros((ncross, nfrecon, nbins, 4))
+#
+# # Get spectra
+# w = None
+# for isub in range(nfrecon):
+#     print('isub:', isub)
+#     cross = 0
+#     for c1 in range(0, nreals - 1, 2):  # do not mix pairs to avoid correlation
+#         c2 = c1 + 1
+#         c = (c1, c2)
+#         print(c)
+#         map1 = maps_clth[c[0], isub, :, :]
+#         map2 = maps_clth[c[1], isub, :, :]
+#         leff, cells_cross[cross, isub, :, :], w = Namaster.get_spectra(map1.T,
+#                                                                        mask_apo,
+#                                                                        map2.T,
+#                                                                        w=w,
+#                                                                        purify_e=True,
+#                                                                        purify_b=False,
+#                                                                        beam_correction=None,
+#                                                                        pixwin_correction=True)
+#         cross += 1
+# with open(rep_save + '/cross_spectrum_nfrecon{}_nreals{}_'.format(nfrecon, nreals) + rnd_name + '.pkl', 'wb') as f:
+#     pickle.dump(cells_cross, f)

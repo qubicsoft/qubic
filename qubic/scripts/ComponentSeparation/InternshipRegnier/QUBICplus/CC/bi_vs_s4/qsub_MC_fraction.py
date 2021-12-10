dfef0555014b669972d16b7c8a67d553c3b62f9d
mathias77515 <56278200+mathias77515@users.noreply.github.com>
2021-12-10 16:39:52 +0100

    Delete qubic/scripts/ComponentSeparation/InternshipRegnier/QUBICplus/CC/bi_vs_s4 directory

--- qubic/scripts/ComponentSeparation/InternshipRegnier/QUBICplus/CC/bi_vs_s4/qsub_MC_fraction.py
+++
@@ -1,9 +0,0 @@
-import os
-import time
-import numpy as np
-
-for i in range(1, 6):
-    print("ite : {}".format(i))
-    #os.system(f'qsub MC_fraction.sh 2 0.5 1.24 1.84 265 145 40 {i} 0')
-    os.system(f'qsub -pe multicores 12 MC_fraction.sh 1 0.5 1.54 1.54 265 145 40 {i} 1')
-    time.sleep(0.1)

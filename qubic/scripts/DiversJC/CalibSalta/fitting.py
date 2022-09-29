1d8359fcd29cc0377b57c6a8924fe54d77bee221
Jean-Christophe Hamilton <jchamilton75@gmail.com>
2022-09-29 09:36:12 +0200

    cleaned sone stuff

--- qubic/scripts/DiversJC/CalibSalta/fitting.py
+++ qubic/scripts/DiversJC/CalibSalta/fitting.py
@@ -2,6 +2,10 @@ from pylab import *
 import numpy as np
 import iminuit
 from iminuit.cost import LeastSquares
+import numba_stats
+from numba_stats import norm
+
+from qubic import fibtools as ft
 
 
 
@@ -37,15 +41,15 @@ class Data:
             legend()
 
 
-    def fit_minuit(self, guess, fixpars = None, limits=None, scan=None, renorm=False, simplex=False):
+    def fit_minuit(self, guess, fixpars = None, limits=None, scan=None, renorm=False, simplex=False, minimizer=LeastSquares):
         ok = np.isfinite(self.x) & (self.errors != 0)
 
         ### Prepare Minimizer
         if self.diag == True:
-            myminimizer = LeastSquares(self.x[ok], self.y[ok], self.errors[ok], self.model)
+            myminimizer = minimizer(self.x[ok], self.y[ok], self.errors[ok], self.model)
         else:
             print('Non diagoal covariance not yet implemented: using only diagonal')
-            myminimizer = LeastSquares(self.x[ok], self.y[ok], self.errors[ok], self.model)
+            myminimizer = minimizer(self.x[ok], self.y[ok], self.errors[ok], self.model)
 
         ### Instanciate the minuit object
         if simplex == False:
@@ -95,4 +99,61 @@ class Data:
             m.errors *= 1./np.sqrt(ch2/ndf)
 
         return m, ch2, ndf
+
+
+def gauss(x, pars):
+    return pars[0]*np.exp(-0.5*(x-pars[1])**2/pars[2]**2)
+
+def gauss_pdf(x, mu, sigma):
+    return (numba_stats.norm.pdf(x, mu, sigma))
+
+
+def myhist(x, unbinned=True, nsig=4, **kwargs):
+    thelabel = None
+    if 'alpha' not in kwargs:
+        kwargs['alpha']=0.5
+    if 'label' in kwargs:
+        thelabel = kwargs['label']
+        kwargs['label'] = ''
+    else:
+        thelabel=''
+    if 'range' not in kwargs:
+        mm, ss = ft.meancut(x, 3)
+        kwargs['range'] = [mm-nsig*ss, mm+nsig*ss]
+        forcerng = False
+    else:
+        forcerng = True
+            
+    if unbinned:
+        c = iminuit.cost.UnbinnedNLL(x, gauss_pdf)
+        mm, ss = ft.meancut(x, 3)
+        m = iminuit.Minuit(c, mu=mm, sigma=ss)
+        m.migrad()
+        m.hesse()
+        mu = m.values['mu']
+        sigma = m.values['sigma']
+        if forcerng is False:
+            kwargs['range'] = [mu-nsig*sigma, mu+nsig*sigma]
+        yy, xe, a = hist(x, **kwargs)
+        xx = 0.5 * (xe[1:] + xe[:-1])
+        dx = np.diff(xe)
+        notzero = yy != 0    
+        p=errorbar(xx[notzero], yy[notzero], yerr=np.sqrt(yy[notzero]), fmt='o', color=a[0].get_facecolor(), alpha=1)
+        xm = np.linspace(np.min(xx), np.max(xx), 100)
+        plot(xm, gauss_pdf(xm, *m.values) * len(x) * dx[0], color=a[0].get_facecolor(), alpha=1, label = thelabel+' {0:5.2g} $\pm$ {1:5.2g}'.format(mu, sigma))
+    else:
+        yy, xe, a = hist(x, **kwargs)
+        xx = 0.5 * (xe[1:] + xe[:-1])
+        dx = np.diff(xe)
+        notzero = yy != 0
+        dd = Data(xx[notzero], yy[notzero], np.sqrt(yy[notzero]), gauss)
+        guess = np.array([len(xx), np.mean(x), np.std(x)])
+        dd.fit_minuit(guess) 
+        mu = dd.fit[1]
+        sigma = dd.fit[2]
+        dd.plot(color=a[0].get_facecolor(), mylabel=None, nostat=True)
+        label = ' {0:5.2g} $\pm$ {1:5.2g}'.format(mu, sigma)
+        a.set_label(thelabel + label)
+        
+    legend(fontsize=10)
 

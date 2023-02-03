71225d4b6868522d3cc3548fbbe6c7b8bfb97241
Steve TORCHINSKY <satorchi@apc.in2p3.fr>
2023-02-03 08:29:05 +0100

    use natural numpy operations instead of looping

--- qubic/level1/saturation.py
+++ qubic/level1/saturation.py
@@ -14,23 +14,27 @@ import numpy as np
 from qubic.level1.flags import flag_bit, set_flag
 
 # This is the value where data flattens out.  See for example: 2019-12-23_19.00.20__HWP_Scanning_Vtes_2TimePerPos_60
-saturation_value = 2**22 - 2**7 
+saturation_value = 2**22 - 2**7
+positive_saturation_value = saturation_value
+negative_saturation_value = -(saturation_value-1)
 saturation_bit = flag_bit['saturation']
 
-def check_saturation(timeline,flag_array=None):
+def check_saturation(todarray,flag_array=None):
     '''
     go through the data, and return the flag array adjusted for saturation
     '''
 
-    nTOD = len(timeline)
+    tod_shape = todarray.shape
     
     # if no flag_array is given, then create it
-    if flag_array is None: flag_array = np.zeros(nTOD,dtype=np.uint)
-
+    if flag_array is None: flag_array = np.zeros(tod_shape,dtype=np.uint)
+    
     # check for saturation
-    for idx,tod in enumerate(timeline):
-        if np.abs(tod)>=saturation_value:
-            flag_array[idx] = set_flag('saturation',flag_array[idx])
+    positive_saturation = todarray >= positive_saturation_value
+    negative_saturation = todarray <= negative_saturation_value
+    saturation = positive_saturation | negative_saturation
+
+    flag_array[saturation] = set_flag('saturation',flag_array[saturation])
 
     return flag_array
 

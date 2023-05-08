7e7c5af0960f2760599676ab837a46aec65d468e
nahuelmg <nahuelmirongranese@gmail.com>
2023-05-08 16:01:15 -0300

    TC update

--- qubic/scripts/time_constants_2023/TC_alldataset.sh
+++ qubic/scripts/time_constants_2023/TC_alldataset.sh
@@ -1,6 +1,28 @@
-#!/bin/bash -l
+#!/bin/bash
+#SBATCH -t 00-12:00
+#SBATCH -m 16GB
+#SBATCH -N 1 # 1 nodes
+#SBATCH -n 32 # 32 tasks
+#SBATCH -c 1 # 1 core per task
 
-#eval "$(/soft/anaconda3/bin/conda shell.bash hook)"
 conda activate myqubic
 
-python time_constants_computation_alldatasets.py
+srun -N 1 -n 1 python ime_constants_computation_alldatasets.py 0 &
+srun -N 1 -n 1 python time_constants_computation_alldatasets.py 1 &
+srun -N 1 -n 1 python time_constants_computation_alldatasets.py 2 &
+srun -N 1 -n 1 python time_constants_computation_alldatasets.py 3 &
+srun -N 1 -n 1 python time_constants_computation_alldatasets.py 4 &
+srun -N 1 -n 1 python time_constants_computation_alldatasets.py 5 &
+srun -N 1 -n 1 python time_constants_computation_alldatasets.py 6 &
+srun -N 1 -n 1 python time_constants_computation_alldatasets.py 7 &
+srun -N 1 -n 1 python time_constants_computation_alldatasets.py 8 &
+srun -N 1 -n 1 python time_constants_computation_alldatasets.py 9 &
+srun -N 1 -n 1 python time_constants_computation_alldatasets.py 10 &
+srun -N 1 -n 1 python time_constants_computation_alldatasets.py 11 &
+srun -N 1 -n 1 python time_constants_computation_alldatasets.py 12 &
+srun -N 1 -n 1 python time_constants_computation_alldatasets.py 13 &
+srun -N 1 -n 1 python time_constants_computation_alldatasets.py 14 &
+srun -N 1 -n 1 python time_constants_computation_alldatasets.py 15 &
+srun -N 1 -n 1 python time_constants_computation_alldatasets.py 16 &
+
+wait
--- qubic/scripts/time_constants_2023/time_constants_computation_alldatasets.py
+++ qubic/scripts/time_constants_2023/time_constants_computation_alldatasets.py
@@ -1,51 +1,44 @@
-# ---
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.14.5
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
-# ---
-
-# +
+import sys
 import os
 import glob
 import numpy as np
 
 from qubic import time_constants_tools as tct
 
+index = int(sys.argv[1])
+
 base_save_path = '/sps/qubic/Users/nahuelmg'
 save_path = base_save_path+'/time_constants_results/alldatasets_to_may_2023/'
 if os.path.exists(save_path):
     print('Save path:',save_path)
 else:
-    print('Insert save path.')
+    raise Exception('Insert save path.')
 
 base_dir = '/sps/qubic/Data/Calib-TD/'
 
 days = ['2019-06-27','2019-07-03','2019-11-12','2019-11-14','2020-07-24','2020-07-25',
-        '2020-07-27','2020-10-16','2020-11-12','2022-04-06','2022-08-18','2022-08-24',
-        '2023-03-02','2023-03-07','2023-03-31','2023-04-03','2023-04-17']
+	'2020-07-27','2020-10-16','2020-11-12','2022-04-06','2022-08-18','2022-08-24',
+	'2023-03-02','2023-03-07','2023-03-31','2023-04-03','2023-04-17']
 
 keywords = ['*TimeCst*','*Fibers*','*New*'+'*TimeCstScript*','*TimeCstScript*','*TimeCstScript*',
-            '*TimeCstScript*','*TimeCstScript*','*TimeCstScript*','*carbonfibre*',
-            '*NoiseMeasurement_FileDuration_CalSourceON180*','*timeconstant_TimePerPos*','*TimeCstScript*',
-            '*calsource','*DomeOpen-Amp*','*CF-0*','*CFiber_square*','*carbon-fiber_0.2*']
+	    '*TimeCstScript*','*TimeCstScript*','*TimeCstScript*','*carbonfibre*',
+	    '*NoiseMeasurement_FileDuration_CalSourceON180*','*timeconstant_TimePerPos*','*TimeCstScript*',
+	    '*calsource','*DomeOpen-Amp*','*CF-0*','*CFiber_square*','*carbon-fiber_0.2*']
 
 fmods = [[0.25],[0.25],[0.6],[0.6],[0.6],[0.6],[0.6],[0.6],[0.8],[0.2],[None],[0.3],[0.2],
-         [0.2, 0.7],[0.6, 0.6, 0.2, 0.2],[0.2],[0.2]]
+	 [0.2, 0.7],[0.6, 0.6, 0.2, 0.2],[0.2],[0.2]]
 
 dcs = [[30],[33.33333],[33.33333],[33.33333],[33.33333],[33.33333],[33.33333],[33.33333],
        [33],[60],[None],[30],[66],[66, 66],[33, 66, 66, 66],[33],[33]]
+       
+days = days[index]
+keywords = keywords[index]
+fmods = fmods[index]
+dcs = dcs[index]
 
-for i,day in enumerate(days):
+for i, (keyword, day) in enumerate(zip(keywords,days)):
     data_dir = base_dir + day + '/'
-    thedirs = np.sort(glob.glob(data_dir+keywords[i]))
+    thedirs = np.sort(glob.glob(data_dir+keyword))
 
     print('Available files to analyze time constants through square modulation corresponding to {}'.format(day))
     

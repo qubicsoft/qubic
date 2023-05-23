3bf915b7cddff211355721900d22a4266ab985df
nahuelmg <nahuelmirongranese@gmail.com>
2023-05-23 15:39:27 -0300

    TC update

--- qubic/scripts/time_constants_2023/TC_alldataset.sh
+++ qubic/scripts/time_constants_2023/TC_alldataset.sh
@@ -2,7 +2,7 @@
 
 conda activate myqubic
 
-srun -N 1 -n 1 python ime_constants_computation_alldatasets.py 0 2 &
+srun -N 1 -n 1 python time_constants_computation_alldatasets.py 0 2 &
 srun -N 1 -n 1 python time_constants_computation_alldatasets.py 2 4 &
 srun -N 1 -n 1 python time_constants_computation_alldatasets.py 4 6 &
 srun -N 1 -n 1 python time_constants_computation_alldatasets.py 6 8 &

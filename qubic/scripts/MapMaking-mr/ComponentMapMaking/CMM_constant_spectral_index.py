f4110eacf71d6f55eb8ca0adaa221a66f150cde6
nahuelmg <nahuelmirongranese@gmail.com>
2023-06-02 11:09:46 -0300

    mapmaking MR scripts

--- qubic/scripts/MapMaking-mr/ComponentMapMaking/CMM_constant_spectral_index.py
+++ qubic/scripts/MapMaking-mr/ComponentMapMaking/CMM_constant_spectral_index.py
@@ -5,7 +5,7 @@ from qubic.data import PATH
 from qubic.io import read_map
 from qubic import QubicSkySim as qss
 import sys
-sys.path.append('/pbs/home/n/nmirongr/qubic/qubic/qubic/scripts/MapMaking-mr')
+sys.path.insert(1,'/pbs/home/n/nmirongr/qubic/qubic/qubic/scripts/MapMaking-mr')
 
 import component_acquisition as Acq
 import pickle

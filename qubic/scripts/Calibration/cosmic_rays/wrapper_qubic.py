83b3e10f894fa82d9452e9b4a7aa691fc81c3904
SofiaFerazzoli <sofiaferazzoli@gmail.com>
2023-06-16 12:21:57 +0200

    Algorithm update for cosmic-rays search: parallelization of time constants search

--- qubic/scripts/Calibration/cosmic_rays/wrapper_qubic.py
+++ qubic/scripts/Calibration/cosmic_rays/wrapper_qubic.py
@@ -16,8 +16,15 @@ def export_data(data_path: str, time_fname: str, signal_raw_fname: str):
     with open(time_fname, "wb") as fout:
         np.save(fout, qubic[0])
 
-    np.savez(signal_raw_fname, **dict(zip(map(str, range(len(qubic[1]))), qubic[1])))
+    # dict(zip(arg1, arg2)) creates a dictionary whose 
+    # keys are represented by arg1 and whose values are represented by arg2
+    np.savez(signal_raw_fname, **dict(zip(map(str, range(len(qubic[1]))), qubic[1]))) 
 
 
 if __name__ == "__main__":
-    export_data(*sys.argv[1:])
+    
+    # I start from index 1 because the first argument is always the name of the script.
+    # wrapper_qubic.py is executed as follows:
+    # python wrapper_qubic.py <.fits files path> <time file path (.npy format)> <signal file path (.npz format)>.
+    # It unpacks the passed arguments and assigns them in order to data_path, time_fname, signal_raw_fname
+    export_data(*sys.argv[1:]) 

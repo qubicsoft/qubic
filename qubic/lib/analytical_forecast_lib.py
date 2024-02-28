a9a2f53e09aa2a5b004c919a00435902e1e51d5d
mathias77515 <mathias.p.regnier@gmail.com>
2024-02-28 12:44:08 +0100

    add comments for each method

--- qubic/lib/analytical_forecast_lib.py
+++ qubic/lib/analytical_forecast_lib.py
@@ -43,42 +43,60 @@ class NoiseEquivalentTemperature:
 
 class AnalyticalForecast:
     
+    '''
+    
+    Instance to produce analytical forecast
+    
+    '''
     
     def __init__(self, nus, NEPdet, NEPpho, Nyrs=3, Nh=400, fsky=0.0182, nside=256):
     
-        ### Check length and type of inputs
+        ### Check type of inputs
         if type(NEPdet) is not list or type(NEPpho) is not list:
             raise TypeError("NEP type should be a list")
         
+        ### Check length of inputs
         if len(NEPdet) != len(NEPpho):
             raise TypeError("NEPdet and NEPpho should have the same length")
         
-        self.nside = nside
+        self.nside = nside                              # Map pixelization
         self.Nyrs = Nyrs
-        self.Tobs = 3600 * 24 * 365 * self.Nyrs
-        self.Nh = Nh
-        self.fsky = fsky
-        self.nus = nus
-        self.nfreqs = len(NEPdet)
+        self.Tobs = 3600 * 24 * 365 * self.Nyrs         # Observation time [s]
+        self.Nh = Nh                                    # Number of horns (detectors for an imager)
+        self.fsky = fsky                                # Observed sky fraction
+        self.nus = nus                                  # Physical bands
+        self.nfreqs = len(NEPdet)                       # Number of frequencies
+        
+        
+        ### Store NEPs
         self.NEPs = np.zeros((self.nfreqs, 2))
         
-        
-        ### Store NEPs and convert them to NETs
         for i in range(self.nfreqs):
             self.NEPs[i, 0] = NEPdet[i]
             self.NEPs[i, 1] = NEPpho[i]
-            #self.NETs[i] = NoiseEquivalentTemperature(self.NEPs[i], self.nus[i]).NETs
         
     def _get_effective_depths(self, NETs):
-    
+        
+        '''
+        
+        Convert Noise Equivalent Temperature in sensitivity depths | muK.sqrt(s) -> muK.arcmin
+        
+        '''
         
         Omega = (4.0 * np.pi * self.fsky) / ((np.pi / (180.0 * 60.0))**2)
         depths = 4 * np.sqrt((Omega * np.power(NETs, 2.)) / (self.Tobs * self.Nh))
-        #depths = np.sqrt((Omega * 2. * np.power(NETs, 2.)) / (self.Tobs * self.Nh))# * (10800. / np.pi)
 
         return depths
     
     def _get_power_spectra(self, depths, A):
+        
+        '''
+        
+        Compute noise power spectrum in each components depending on size of A, assumed to be white. 
+        
+                    Nl = (A.T . N^{-1} . A)^{-1}
+        
+        '''
         
         fwhm = np.array([0.0041, 0.0041])
         bl = np.array([hp.gauss_beam(b, lmax=2*self.nside) for b in fwhm])
@@ -92,10 +110,23 @@ class AnalyticalForecast:
         return Nl
 
     def _fisher(self, ell, Nl):
+        
+        '''
+        
+        Fisher to compute sigma(r) for a given noise power spectrum.
+        
+        '''
+        
         ClBB = give_cl_cmb(ell, r=1, Alens=0.)
         return np.sum(((2 * ell + 1)/2) * self.fsky * (ClBB / Nl)**2)**(-0.5)
     
-    def _get_sigr(self, A, ell):
+    def main(self, A, ell):
+        
+        '''
+        
+        Method to convert NEPs to sigma(r).
+        
+        '''
         
         ### NEPs [W/sqrt(Hz)] -> NETs [muK.sqrt(s)]
         NETs = np.zeros(self.nfreqs)
@@ -114,14 +145,7 @@ class AnalyticalForecast:
         sigr = self._fisher(ell, Nl)
         print(sigr)
         
+        return sigr
         
         
         
-        
-        
-ell = np.array([40.5, 70.5, 100.5, 130.5, 160.5, 190.5, 220.5, 250.5, 280.5, 310.5, 340.5, 370.5, 400.5, 430.5, 460.5])
-A = np.array([[1],
-              [1]])   
- 
-af = AnalyticalForecast([150, 220], [4.7e-17, 4.7e-17], [4.55e-17, 1.16e-16], Nyrs=3, Nh=400, fsky=0.0182, nside=256)
-af._get_sigr(A, ell)

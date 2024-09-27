6e6368ad8647f802a8bac03a17e262425fde5b72
TomLaclavere <laclavere@apc.in2p3.fr>
2024-09-27 16:15:33 +0200

    Build documentation for qubicsoft

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ extensions = [
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+add_module_names = False
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output

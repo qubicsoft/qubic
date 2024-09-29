0f5b85b54cbb422d90b3576ea76bd3f72f366412
mathias77515 <mathias.p.regnier@gmail.com>
2024-09-29 23:26:48 +0200

    update fit X-spec

--- qubic/scripts/MapMaking/src/run_fit_fmm.py
+++ qubic/scripts/MapMaking/src/run_fit_fmm.py
@@ -7,28 +7,31 @@ from qubic.lib.Qfoldertools import MergeAllFiles
 from qubic.lib.MapMaking.FrequencyMapMaking.Qspectra_component import SkySpectra
 from qubic.lib.Qmpi_tools import MpiTools
 
-
-
 ############################
 ######## Parameters ########
 ############################
 
 ### Folder where spectrum should be stored
-folder_spectrum = '/Users/mregnier/Desktop/installation_qubic/qubic/qubic/scripts/MapMaking/src/FMM/cmbdust_nrec2/spectrum/'
+folder_spectrum = 'path/to/your/data/folder'
 
 ### Parameters file
-parameters_file = '/Users/mregnier/Desktop/installation_qubic/qubic/qubic/scripts/MapMaking/src/FMM/configuration_files/fit_params.txt'
+parameters_file = 'path/to/your/parameter_file.txt'
 
 ### Number of multipole (minimum is fixed at what you defined during the map-making)
-NBINS = 8
+NBINS = 10
 SAMPLE_VARIANCE = True
+DIAGONAL = False
+FSKY = 0.01
+DL = 30
 
 ### Which frequency you want ot choose ?
-nus_index = np.array([True, True, False, False, False, False, False, False, False])
+nus_index = np.array([True, True, True, True,
+                      False, False, False, False, False, True, True])
 
-DISCARD = 150
-NWALKERS = 30
-NSTEPS = 300
+DISCARD = 100
+NWALKERS = 10
+NSTEPS = 200
+VERBOSE = False
 
 ############################
 
@@ -37,7 +40,7 @@ mpi = MpiTools(comm)
 
 ### Concatenate all realizations
 files = MergeAllFiles(folder_spectrum)
-
+mpi._print_message(f'Number of realizations : {files.number_of_realizations}')
 ### Multipoles
 mpi._print_message('    => Reading multipoles')
 ell = files._reads_one_file(0, "ell")[:NBINS]
@@ -45,8 +48,10 @@ ell = files._reads_one_file(0, "ell")[:NBINS]
 ### Frequencies
 mpi._print_message('    => Reading frequencies')
 nus = files._reads_one_file(0, "nus")[nus_index]
+mpi._print_message(f'nus : {nus}')
 
-BBsignal = np.mean(files._reads_all_files("Dls"), axis=0)[nus_index, :, :NBINS][:, nus_index, :NBINS]
+
+BBsignal = np.mean(files._reads_all_files("Dls", verbose=VERBOSE), axis=0)[nus_index, :, :NBINS][:, nus_index, :NBINS]
 BBnoise = files._reads_all_files("Nl")[:, :, nus_index, :NBINS][:, nus_index, :, :NBINS]
 
 ### Remove noise bias
@@ -58,7 +63,7 @@ sky = SkySpectra(ell, nus)
 
 ### Fit of cosmological parameters
 mpi._print_message('    => Fitting parameters')
-fit = FitEllSpace(ell, BBsignal, BBnoise, model=sky.model, parameters_file=parameters_file, sample_variance=SAMPLE_VARIANCE)
+fit = FitEllSpace(ell, BBsignal, BBnoise, model=sky.model, parameters_file=parameters_file, sample_variance=SAMPLE_VARIANCE, fsky=FSKY, dl=DL, diagonal=DIAGONAL)
 samples, samples_flat = fit.run(NSTEPS, NWALKERS, discard=DISCARD, comm=comm)
 
 plt.figure()

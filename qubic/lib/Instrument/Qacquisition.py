9ab8492552416c501c6ed14f9316dfaaf5b3f05f
TomLaclavere <laclavere@apc.in2p3.fr>
2025-09-26 17:37:31 +0200

    Fix Nsub_planck shape issue

--- qubic/lib/Instrument/Qacquisition.py
+++ qubic/lib/Instrument/Qacquisition.py
@@ -1385,7 +1385,7 @@ class PlanckAcquisitionTest:
         for _ in self.nus:
             ope_i = []
             # for _ in range(self.nsub_planck):
-            for i in range(int(self.nsub_planck / len(self.nus))):
+            for i in range(self.nsub_planck):  # int(self.nsub_planck / len(self.nus))):
                 if fwhm is not None:
                     C = HealpixConvolutionGaussianOperator(fwhm=fwhm[k], lmax=3 * self.nside - 1)
                 else:

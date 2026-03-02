b9b3aacdb973ee38306414413f8c6be0fc04d955
TomLaclavere <laclavere@apc.in2p3.fr>
2026-03-02 09:59:44 +0100

    fix fsky parameter in FMM

--- qubic/lib/MapMaking/FrequencyMapMaking/Qfmm.py
+++ qubic/lib/MapMaking/FrequencyMapMaking/Qfmm.py
@@ -123,6 +123,7 @@ class PipelineFrequencyMapMaking:
         self.coverage = self.joint.qubic.subacqs[0].get_coverage()
         self.seenpix = self.coverage / self.coverage.max() > self.params["SKY"]["coverage_cut"]
         self.seenpix_qubic = self.coverage / self.coverage.max() > 0
+        self.fsky = np.sum(self.seenpix) / self.seenpix.shape[0]
 
         ### Angular resolutions
         self.fwhm_in, self.fwhm_out, self.fwhm_rec = self.get_convolution()
